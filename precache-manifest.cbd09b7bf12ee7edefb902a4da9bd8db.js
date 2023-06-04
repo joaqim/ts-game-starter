07490655ceb8b3b66f128ec5dbb239bc09e03ceb
Joaqim Planstedt <git@joaqim.xyz>
2023-06-04 22:18:40 +0200

    Updates

---
+++ precache-manifest.cbd09b7bf12ee7edefb902a4da9bd8db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb7330d40bfdf43c18df6aec0aa09661",
+    "url": "/ts-game-starter/index.html"
+  },
+  {
+    "revision": "f9c642b260def4d23307",
+    "url": "/ts-game-starter/static/js/2.cc51407a.chunk.js"
+  },
+  {
+    "revision": "555486c5c14e9d19a66b1506a7ff28b9",
+    "url": "/ts-game-starter/static/js/2.cc51407a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08142e9a6dfc7f0b2b71",
+    "url": "/ts-game-starter/static/js/main.54db5d4f.chunk.js"
+  },
+  {
+    "revision": "50f4fffe9b22c2dd42e2",
+    "url": "/ts-game-starter/static/js/runtime-main.837e6999.js"
+  }
+]);
